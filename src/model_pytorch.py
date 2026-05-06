1269a2207496b424381a05540546cb8d621dbe86
Armin Golzar <armingolzar78@gmail.com>
2026-05-06 16:10:35 +0330

    model_pytorch.py is done

--- src/model_pytorch.py
+++ src/model_pytorch.py
@@ -16,4 +16,33 @@ class Transfer_learning_Cifar(nn.Module):
         )
 
         vgg16 = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
-        self.vgg16_encoding = vgg16.features
+        self.vgg16_encoding = vgg16.features[:17]
+
+
+        for index, prams in enumerate(self.vgg16_encoding.children()):
+            if index <= 16:
+                for pram in prams.parameters():
+                    pram.requires_grad = False
+
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(16384, 256)
+        self.relu = nn.ReLU()
+        self.drop = nn.Dropout(p=0.3)
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+
+        if self.training:
+            x = self.augmentation(x)
+
+        x = self.vgg16_encoding(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+
+        return x 
+    
+    
+
